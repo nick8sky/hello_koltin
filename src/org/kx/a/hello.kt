c46bc805442b5820f897ae2cd132663779ace17f
sunkaixiang <kaixiang.sun@funlintechfin.com>
2018-02-20 22:08:24 +0800

    d

--- src/org/kx/a/hello.kt
+++ src/org/kx/a/hello.kt
@@ -4,6 +4,12 @@ package org.kx.a
 fun main(args: Array<String>) {    // 包级可见的函数，接受一个字符串数组作为参数
     println("Hello World!")         // 分号可以省略
     Greeter("nick").greet()
+
+    if (args.size == 0) {
+        println("Please provide a name as a command-line argument")
+        return
+    }
+    println("Hello, ${args[0]}!")
 }
 
 
